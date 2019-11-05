1caeb6ef0283356fb6d8d6da2512b84aa7945725
johanna <johanna_lim@dlsu.edu.ph>
2019-11-06 00:46:20 +0800

    update

---
+++ app/src/main/java/com/example/bucketlist/NoteDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.bucketlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+    }
+
+
+}
