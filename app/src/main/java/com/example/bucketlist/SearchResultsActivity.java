27abccc273d944af68cff3a7316f824be23b05e2
johanna <johanna_lim@dlsu.edu.ph>
2019-11-05 23:58:49 +0800

    search

--- app/src/main/java/com/example/bucketlist/SearchResultsActivity.java
+++ app/src/main/java/com/example/bucketlist/SearchResultsActivity.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SearchResultsActivity extends AppCompatActivity {
+
+    LinearLayout listLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,35 @@ public class SearchResultsActivity extends AppCompatActivity {
             String query = intent.getStringExtra(SearchManager.QUERY);
             Toast.makeText(getApplicationContext(), "You searched for: " + query, Toast.LENGTH_LONG).show();
         }
+
+        listLayout = findViewById(R.id.listLayout);
+//        for(every search item){
+//            displayResults(name, category);
+//        }
+    }
+
+    public void displayResults(String name, String category){
+        LinearLayout newLayout = new LinearLayout(this);
+        newLayout.setOrientation(LinearLayout.HORIZONTAL);
+        newLayout.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        final TextView nameField = new TextView(this);
+        nameField.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        nameField.setText(name);
+
+        final TextView catField = new TextView(this);
+        catField.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        catField.setText(category);
+
+        newLayout.addView(nameField);
+        newLayout.addView(catField);
+
+        listLayout.addView(newLayout);
     }
 }
