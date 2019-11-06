8a055f2fa88e4ed83e36a65840cf00a42f457281
johanna <johanna_lim@dlsu.edu.ph>
2019-11-06 23:17:19 +0800

    manual add activity skeleton

--- app/src/main/java/com/example/bucketlist/ManualAddActivity.java
+++ app/src/main/java/com/example/bucketlist/ManualAddActivity.java
@@ -3,12 +3,44 @@ package com.example.bucketlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class ManualAddActivity extends AppCompatActivity {
+    private RadioGroup categoryChoices;
+    private RadioButton selectedCategory;
+    private Button addBtn, backBtn;
+    private EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manual_add);
+        setContentView(R.layout.activity_main);
+
+        categoryChoices = (RadioGroup) findViewById(R.id.categoryChoices);
+        addBtn = (Button) findViewById(R.id.addBtn);
+//        backBtn = (Button) findViewById(R.id.backBtn);
+        name = (EditText) findViewById(R.id.name);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // get selected radio button from radioGroup
+                int selectedId = categoryChoices.getCheckedRadioButtonId();
+                selectedCategory = (RadioButton) findViewById(selectedId);
+
+                Toast.makeText(ManualAddActivity.this,
+                        selectedCategory.getText() + name.getText().toString(), Toast.LENGTH_SHORT).show();
+                // add new Note object into DB
+            }
+        });
+
+//        backBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // go back to search screen
+//            }
+//        });
     }
 }
