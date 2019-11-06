9ac1c994834ac352555c2f50432fd8f3be4de640
johanna <johanna_lim@dlsu.edu.ph>
2019-11-06 15:59:55 +0800

    fragments and other activities

---
+++ app/src/main/java/com/example/bucketlist/ManualAddActivity.java
@@ -0,0 +1,14 @@
+package com.example.bucketlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManualAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_add);
+    }
+}
--- app/src/main/java/com/example/bucketlist/NoteDetailsActivity.java
+++ app/src/main/java/com/example/bucketlist/NoteDetailsActivity.java
@@ -1,16 +1,43 @@
 package com.example.bucketlist;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Visibility;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class NoteDetailsActivity extends AppCompatActivity {
+
+    TextView name, descrription;
+    ArrayList<String> details;
+    Button deleteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_details);
+        name = (TextView) findViewById(R.id.name);
+        descrription = (TextView) findViewById(R.id.description);
+        Intent intent = getIntent();
+        details = intent.getStringArrayListExtra("NOTE_DETAILS");
+        displayDetails(details);
+        deleteBtn = (Button) findViewById(R.id.deleteBtn);
     }
 
+    public void displayDetails(ArrayList<String> details){
+        name.setText(details.get(0));
+        descrription.setText(details.get(1));
+    }
 
+    public void add(View v){
+
+    }
+
+    public void remove(View v){
+
+    }
 }
